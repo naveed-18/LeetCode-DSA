032fd0e9e281f340c33398fca608f1866a59b140
Naveed <23211a05k3@bvrit.ac.in>
2026-06-17 19:29:50 +0530

    Time: 3 ms (87.89%), Space: 46.6 MB (28.5%) - LeetHub

--- 0748-shortest-completing-word/0748-shortest-completing-word.java
+++ 0748-shortest-completing-word/0748-shortest-completing-word.java
@@ -1,27 +1,32 @@
 class Solution {
     public String shortestCompletingWord(String licensePlate, String[] words) {
-        String result = "";
-        int resLen = Integer.MAX_VALUE;
+        licensePlate = licensePlate.toLowerCase();
+        int[] target = new int[26];
+        for (char c : licensePlate.toCharArray()) {
+            if (Character.isLetter(c)) {
+                target[c - 'a']++;
+            }
+        }
+
+        String result = null;
+        int minLength = Integer.MAX_VALUE;
         for (String word : words) {
-            if (isCompletingWord (word, licensePlate) && word.length() < resLen) {
+            if (isMatch(word, target) && word.length() < minLength) {
+                minLength = word.length();
                 result = word;
-                resLen = word.length();
             }
         }
         return result;
     }
-
-    public boolean isCompletingWord(String word, String lp) {
-        int[] freq = new int[26];
-        for (char c : lp.toCharArray()) {
-            if (c == ' ' || Character.isDigit(c)) continue;
-            freq[Character.toLowerCase(c) - 'a']++;
-        }
+    public boolean isMatch(String word, int[] target) {
+        int[] count = new int[26];
         for (char c : word.toCharArray()) {
-            freq[c - 'a']--;
+            count[c - 'a']++;
         }
         for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) return false;
+            if (count[i] < target[i]) {
+                return false;
+            }
         }
         return true;
     }
