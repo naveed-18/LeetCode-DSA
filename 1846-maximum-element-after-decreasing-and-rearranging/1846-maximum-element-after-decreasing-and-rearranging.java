1560de93f4134e2333aaff54216c90b9879c1ddd
Naveed <23211a05k3@bvrit.ac.in>
2026-06-28 14:21:55 +0530

    Time: 11 ms (12.08%), Space: 77.4 MB (78.75%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -4,7 +4,7 @@ class Solution {
         int n = nums.length;
         nums[0] = 1;
         for (int i = 1; i < n; i++) {
-            if (Math.abs(nums[i] - nums[i - 1]) > 1) {
+            if (nums[i] > nums[i - 1] + 1) {
                 nums[i] = nums[i - 1] + 1;
             }
         }
