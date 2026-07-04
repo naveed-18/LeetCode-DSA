4c1d3892edef67d1da65417f4f15875fb80795e8
Naveed <23211a05k3@bvrit.ac.in>
2026-07-04 11:58:37 +0530

    Time: 139 ms (24.55%), Space: 145.6 MB (5.07%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -0,0 +1,76 @@
+class Solution {
+    class Pair {
+        int num, index;
+        Pair (int num, int index) {
+            this.num = num;
+            this.index = index;
+        } 
+    }
+    public List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+
+        Pair[] numsPair = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            numsPair[i] = new Pair(nums[i], i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) result.add(0);
+
+        mergeSort(0, n - 1, numsPair, result);
+
+        return result;
+    } 
+
+    public void mergeSort(int low, int high, Pair[] numsPair, List<Integer> result) {
+        if (low >= high) return;
+
+        int mid = low + (high - low) / 2;
+        mergeSort(low, mid, numsPair, result);
+        mergeSort(mid + 1, high, numsPair, result);
+        countSmallNumbers(low, mid, high, numsPair, result);
+
+        merge(low, mid, high, numsPair);
+    }
+
+    public void countSmallNumbers(int low, int mid, int high, 
+        Pair[] numsPair, List<Integer> result) {
+
+        int j = mid + 1;
+
+        for (int i = low; i <= mid; i++) {
+            while (j <= high && numsPair[i].num > numsPair[j].num) j++;
+
+            result.set(numsPair[i].index, result.get(numsPair[i].index) + (j - (mid + 1)));
+        }
+    }
+
+    public void merge(int low, int mid, int high, Pair[] numsPair) {
+        Pair[] temp = new Pair[high - low + 1];
+        int i = low, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= high) {
+            if (numsPair[i].num <= numsPair[j].num) {
+                temp[k++] = new Pair(numsPair[i].num, numsPair[i].index);
+                i++;
+            } else {
+                temp[k++] = new Pair(numsPair[j].num, numsPair[j].index);
+                j++;
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = new Pair(numsPair[i].num, numsPair[i].index);
+            i++;
+        }
+
+        while (j <= high) {
+            temp[k++] = new Pair(numsPair[j].num, numsPair[j].index);
+            j++;
+        }
+
+        for (int idx = low; idx <= high; idx++) {
+            numsPair[idx] = temp[idx - low];
+        }
+    }
+}
