0d83f17006b26809f36e919bef8d6b52ddac451f
Naveed <23211a05k3@bvrit.ac.in>
2026-07-04 08:20:53 +0530

    Time: 27 ms (64.91%), Space: 162 MB (67.32%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -35,7 +35,46 @@ class DisjointSet {
 
 class Solution {
     public int minScore(int n, int[][] roads) {
-        return minScoreDFS(n, roads);
+        return minScoreBFS(n, roads);
+    }
+
+    public int minScoreBFS(int n, int[][] roads) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge : roads) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            adj.get(u).add(new int[]{v, wt});
+            adj.get(v).add(new int[]{u, wt});
+        }
+        
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        visited[1]=true;
+
+        int result = Integer.MAX_VALUE;
+
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+
+            for(int[] edge :adj.get(curr)) {
+                int v = edge[0];
+                int wt = edge[1];
+
+                result = Math.min(result, wt);
+                if(!visited[v]) {
+                    visited[v] = true;
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return result;
     }
 
     public int minScoreDFS(int n, int[][] roads) {
