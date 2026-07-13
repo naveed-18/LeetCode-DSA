bfa5fa239e4e695e3d30ca3f74908a20195689af
Naveed <mdnaveedkhan18@gmail.com>
2026-07-13 06:57:13 +0530

    Time: 0 ms (100%), Space: 42.4 MB (36.36%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,27 +1,20 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-        int[] ans = {12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789, 12345678, 23456789, 123456789};
+        List<Integer> ans = new ArrayList<>();
 
-        int lowIdx = -1, highIdx = -1;
-        for (int i = 0; i < ans.length; i++) {
-            if (low <= ans[i] && lowIdx == -1) {
-                lowIdx = i;
-            }
-            if (high < ans[i]) {
-                highIdx = i;
-                break;
+        String s = "123456789";
+        String l = String.valueOf(low);
+        String h = String.valueOf(high);
+
+        for (int len = l.length(); len <= h.length(); len++) {
+            for (int start = 0; start <= 9 - len; start++) {
+                int num = Integer.parseInt(s.substring(start, start + len));
+                if (num >= low && num <= high) {
+                    ans.add(num);
+                }
             }
         }
 
-        List<Integer> result = new ArrayList<>();
-
-        if (highIdx == -1) highIdx = ans.length;
-        if (lowIdx == -1) return result;
-
-        for (int i = lowIdx; i < highIdx; i++) {
-            result.add(ans[i]);
-        }
-
-        return result;
+        return ans;
     }
 }
