c6b1b221e2231d0caba1875e48bfbd6a0195b776
Naveed <mdnaveedkhan18@gmail.com>
2026-07-14 21:17:23 +0530

    Time: 73 ms (24.81%), Space: 46.2 MB (41.41%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,30 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        // return lis (0, nums, nums.length, -1);
         int n = nums.length;
-        int[][] dp = new int[n][n + 1];
-        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
-        return lisMemo (0, nums, n, -1, dp);
+        return tabulationSpaceOptimized(nums, n);
     }
+    public int tabulationSpaceOptimized (int[] nums, int n) {
+        int[] next = new int[n + 1];
+        int[] curr = new int[n + 1];
 
-    public int lis (int idx, int[] nums, int n, int lastIdx) {
-        if (idx >= n) return 0;
-        int notTake = lis(idx + 1, nums, n, lastIdx);
-        int take = 0;
-        if (lastIdx == -1 || nums[lastIdx] < nums[idx]) {
-            take = 1 + lis (idx + 1, nums, n, idx);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prevIdx = i - 1; prevIdx >= -1; prevIdx--) {
+                int pick = 0;
+                if (prevIdx == -1 || nums[i] > nums[prevIdx]) {
+                    pick = 1 + next[i + 1];
+                } 
+                int notPick = 0 + next[prevIdx + 1];
+                curr[prevIdx + 1] = Math.max(pick, notPick);
+            }
+            next = curr;
         }
-        return Math.max(take, notTake);
-    }
 
-    public int lisMemo (int idx, int[] nums, int n, int lastIdx, int[][] dp) {
-        if (idx >= n) return 0;
-        if (dp[idx][lastIdx + 1] != -1) return dp[idx][lastIdx + 1];
-        int notTake = lisMemo (idx + 1, nums, n, lastIdx, dp);
-        int take = 0;
-        if (lastIdx == -1 || nums[lastIdx] < nums[idx]) {
-            take = 1 + lisMemo (idx + 1, nums, n, idx, dp);
-        }
-        return dp[idx][lastIdx + 1] = Math.max(take, notTake);
+        return next[0];        
     }
 }
