3b9a44b6a571380864fd388ff55cc7e1e655a02f
Naveed <mdnaveedkhan18@gmail.com>
2026-08-03 11:39:03 +0530

    Time: 43 ms (31.25%), Space: 86.2 MB (58.24%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,10 +1,11 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
-        int[][] dp = new int[n][2];
-        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+        // int[][] dp = new int[n][2];
+        // for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+        // int result = stoneGame (0, stoneValue, n, 1, dp);
 
-        int result = stoneGame (0, stoneValue, n, 1, dp);
+        int result = stoneGameTabulation (stoneValue, n);
         if (result > 0) return "Alice";
         else if (result < 0) return "Bob";
         return "Tie";
@@ -37,5 +38,35 @@ class Solution {
         }
 
         return dp[idx][isAliceTurn] = ans;
+    }
+
+    public int stoneGameTabulation (int[] stoneValue, int n) {
+        int[][] dp = new int[n + 1][2];
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int isAliceTurn = 0; isAliceTurn < 2; isAliceTurn++) {
+                int ans = isAliceTurn == 1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                int sum = 0;
+                for (int i = 0; i < 3; i++) {
+                    if (idx + i < n) {
+                        sum += stoneValue[idx + i];
+                        if (isAliceTurn == 1) {
+                            ans = Math.max (
+                                ans,
+                                sum + dp[idx + i + 1][0]
+                            );
+                        } else {
+                            ans = Math.min (
+                                ans,
+                                -sum + dp[idx + i + 1][1]
+                            );
+                        }
+                    }
+                }
+                dp[idx][isAliceTurn] = ans; 
+            }
+        }
+
+        return dp[0][1];
     }
 }
