acb7416b73353635f8f1a9f634152f71ea96214a
Naveed <mdnaveedkhan18@gmail.com>
2026-08-06 20:59:31 +0530

    Time: 2 ms (94.02%), Space: 64.9 MB (81.19%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] leftProd = new int[n];
+        int[] rightProd = new int[n];
+        int[] result = new int[n];
+
+        leftProd[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftProd[i] = leftProd[i - 1] * nums[i];
+        }
+
+        rightProd[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightProd[i] = rightProd[i + 1] * nums[i];
+        }
+
+        
+        result[0] = rightProd[1];
+        result[n - 1] = leftProd[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            result[i] = leftProd[i - 1] * rightProd[i + 1];
+        }
+
+        return result;
+    }
+}
