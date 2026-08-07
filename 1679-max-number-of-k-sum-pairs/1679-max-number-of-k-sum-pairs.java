d73263e62c20a727e3da7535e832f89fae78ae87
Naveed <mdnaveedkhan18@gmail.com>
2026-08-07 09:42:57 +0530

    Time: 36 ms (31.86%), Space: 67.1 MB (98.84%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,19 +1,39 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int low = 0, high = n - 1;
-        int res = 0;
-        while (low < high) {
-            int sum = nums[low] + nums[high];
-            if (sum == k) {
-                res++;
-                low++;
-                high--;
-            } else if (sum < k) low++;
-            else high--;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int maxOpr = 0;
+
+        for (int num : nums) {
+            int need = k - num;
+            if (hm.containsKey(need)) {
+                maxOpr++;
+                if (hm.get(need) == 1) hm.remove(need);
+                else hm.put(need, hm.get(need) - 1);
+            } else {
+                hm.put(num, hm.getOrDefault(num, 0) + 1);
+            }
         }
 
-        return res;
+        return maxOpr;
+    }
+
+    public int maxOperationsSorting(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int maxOpr = 0;
+        int n = nums.length;
+        int i = 0, j = n - 1;
+
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == k) {
+                maxOpr++;
+                i++;
+                j--;
+            } else if (sum < k) i++;
+            else j--;
+        }
+
+        return maxOpr;
     }
 }
