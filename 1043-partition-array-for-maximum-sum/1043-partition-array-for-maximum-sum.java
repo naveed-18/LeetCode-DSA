f4ef77d895d16cc4032516294d90ccc09989b77f
Naveed <mdnaveedkhan18@gmail.com>
2026-08-19 06:49:42 +0530

    Time: 6 ms (94.58%), Space: 44 MB (46.62%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,10 +1,28 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] nums, int k) {
+        int n = nums.length;
         // return partitionArrayMaxSum (0, nums, k);
 
-        int n = nums.length;
-        Integer[] dp = new Integer[n];
-        return partitionArrayMaxSumMemo (0, nums, k, dp);
+        // Integer[] dp = new Integer[n];
+        // return partitionArrayMaxSumMemo (0, nums, k, dp);
+
+        return partitionArrayMaxSumTab (nums, k, n);
+    }
+
+    public int partitionArrayMaxSumTab (int[] nums, int k, int n) {
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int maxValue = nums[i];
+            int maxSum = Integer.MIN_VALUE;
+            for (int j = i; j < i + k && j < nums.length; j++) {
+                maxValue = Math.max(maxValue, nums[j]);
+                maxSum = Math.max(maxSum, (maxValue * (j - i + 1)) + dp[j + 1]);
+            }
+            dp[i] = maxSum;
+        }
+
+        return dp[0];
     }
 
     public int partitionArrayMaxSumMemo (int i, int[] nums, int k, Integer[] dp) {
